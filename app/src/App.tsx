fb8fba1c4481c755bb0fc86650f07409902340cb
k70suK3-k06a7ash1 <2814109k@gmail.com>
2025-01-19 16:30:10 +0900

    chore

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,14 +1,13 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react'
+
 import './App.css'
 import { Octokit } from 'octokit';
+import { Button } from './components/ui/button';
 
 
 const octokit = new Octokit({ auth: import.meta.env.VITE_APP_GITHUB_TOKEN });
 
 function App() {
-  const [count, setCount] = useState(0)
 
 
   useEffect(() => {
@@ -21,40 +20,12 @@ function App() {
 
       const repos = await octokit.rest.repos.listForUser({type: "all",per_page: 100, username:import.meta.env.VITE_APP_USER_NAME})
       console.log({repos})
-      // console.log({token : import.meta.env.VITE_APP_GITHUB_TOKEN})
-      // console.log({user: import.meta.env.VITE_APP_USER_NAME})
-      // const response = await fetch(`https://api.github.com/repos/${import.meta.env.VITE_APP_USER_NAME!}`, {
-      //   headers: {
-      //     Authorization: `Bearer ${import.meta.env.VITE_APP_GITHUB_TOKEN!}`,
-      //     'Content-Type': 'application/json',
-      //   },
-      // });
-
-      // console.log({response:await response.json()})
+     
     })()
   },[])
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Button>hello</Button>
     </>
   )
 }
