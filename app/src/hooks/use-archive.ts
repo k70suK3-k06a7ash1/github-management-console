de6cb0bf31b3fed8fc2fb7cfd589bf6ba58e54ef
k70suK3-k06a7ash1 <2814109k@gmail.com>
2025-01-19 17:41:57 +0900

    chore

--- app/src/hooks/use-archive.ts
+++ app/src/hooks/use-archive.ts
@@ -1,6 +1,6 @@
 import { handleArchive } from "@/helpers"
 import { useState } from "react"
-
+import { toast } from "sonner"
 export const useArchive = () => {
     const [isLoading , setIsLoading ] = useState<boolean>(false)
 
@@ -8,6 +8,7 @@ export const useArchive = () => {
         try{
             setIsLoading(true)
            await  handleArchive(repoName)
+           toast("Repository has been archived.")
         }catch(_e){}finally{
             setIsLoading(false)
         }
