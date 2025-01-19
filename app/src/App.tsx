11de8f3968bfd5a527cad87e913d83460f754e33
k70suK3-k06a7ash1 <2814109k@gmail.com>
2025-01-19 17:17:23 +0900

    chore

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -5,13 +5,52 @@ import { GithubRepositoriesTable, GithubRepository } from './components/GithubRe
 
 const octokit = new Octokit({ auth: import.meta.env.VITE_APP_GITHUB_TOKEN });
 
+// リポジトリを再帰的に取得する関数
+type RepoData = Awaited<
+  ReturnType<
+    Octokit["rest"]["repos"]["listForUser"]
+  >
+>["data"];
+
+const fetchAllRepositories = async (
+  octokit: Octokit,
+  page: number,
+  perPage: number,
+  accumulator: RepoData = []
+): Promise<RepoData> => {
+  const response = await octokit.rest.repos.listForUser({
+    per_page: perPage,
+    page,
+    username:import.meta.env.VITE_APP_USER_NAME
+  });
+
+  if (response.data.length === 0) {
+    return accumulator;
+  }
+  const newAccumulator = [...accumulator, ...response.data];
+  return fetchAllRepositories(octokit, page + 1, perPage, newAccumulator);
+};
+
+// エントリーポイント関数
+const getAllRepositories = async (): Promise<RepoData> => {
+  const initialPage = 1;
+  const perPage = 100;
+    try{
+        return await fetchAllRepositories(octokit, initialPage, perPage);
+    } catch (error) {
+    console.error("An error occurred:", error);
+    return [];
+    }
+};
+
 function App() {
   const [repos ,setRepos] = useState<GithubRepository[]>([])
 
   useEffect(() => {
     (async() => {
-      const repos = await octokit.rest.repos.listForUser({type: "all",per_page: 100, username:import.meta.env.VITE_APP_USER_NAME})
-      const dataSorce: GithubRepository[] = repos?.data?.map((e) => ({id: e.id, repo: e.name, visibility :e.visibility ?? "unknown" })) 
+      const repos = await getAllRepositories()
+      console.log({repos})
+      const dataSorce: GithubRepository[] = repos?.map((e) => ({id: e.id, repo: e.name, visibility :e.visibility ?? "unknown" })) 
       setRepos(dataSorce??[] )
      
     })()
