e1462f068c5fedd873634ab4812f25cf43de03ba
k70suK3-k06a7ash1 <2814109k@gmail.com>
2025-01-19 16:25:33 +0900

    chore

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,38 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Octokit } from 'octokit';
+
+
+const octokit = new Octokit({ auth: import.meta.env.VITE_APP_GITHUB_TOKEN });
 
 function App() {
   const [count, setCount] = useState(0)
 
+
+  useEffect(() => {
+    (async() => {
+
+      const {
+        data: { login },
+      } = await octokit.rest.users.getAuthenticated();
+      console.log("Hello, %s", login);
+
+      const repos = await octokit.rest.repos.listForUser({type: "all",per_page: 100, username:import.meta.env.VITE_APP_USER_NAME})
+      console.log({repos})
+      // console.log({token : import.meta.env.VITE_APP_GITHUB_TOKEN})
+      // console.log({user: import.meta.env.VITE_APP_USER_NAME})
+      // const response = await fetch(`https://api.github.com/repos/${import.meta.env.VITE_APP_USER_NAME!}`, {
+      //   headers: {
+      //     Authorization: `Bearer ${import.meta.env.VITE_APP_GITHUB_TOKEN!}`,
+      //     'Content-Type': 'application/json',
+      //   },
+      // });
+
+      // console.log({response:await response.json()})
+    })()
+  },[])
   return (
     <>
       <div>
