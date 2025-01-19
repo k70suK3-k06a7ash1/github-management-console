c3553026fffede118889991f69f9ecfb2971e6fd
k70suK3-k06a7ash1 <2814109k@gmail.com>
2025-01-19 17:45:44 +0900

    chore

--- app/src/components/Spinner.tsx
+++ app/src/components/Spinner.tsx
@@ -1,10 +1,7 @@
-// Spinner.tsx
-
-export const Spinner= () => {
-  return (
-    <div className="fixed top-0 left-0 w-full h-full bg-gray-900 bg-opacity-50 flex justify-center items-center z-50">
-      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 border-solid"></div>
-    </div>
-  );
+export const Spinner = () => {
+	return (
+		<div className="fixed top-0 left-0 w-full h-full bg-gray-900 bg-opacity-50 flex justify-center items-center z-50">
+			<div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 border-solid" />
+		</div>
+	);
 };
-
--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,12 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { Toaster } from "@/components/ui/sonner"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { Toaster } from "@/components/ui/sonner";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-    <Toaster />
-  </StrictMode>,
-)
+// biome-ignore lint/style/noNonNullAssertion: <explanation>
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<App />
+		<Toaster />
+	</StrictMode>,
+);
