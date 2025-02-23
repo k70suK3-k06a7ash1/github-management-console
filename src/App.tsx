f157b2a7824266ef6099fdbd6d462223f7e5bc9d
k70suK3-k06a7ash1 <2814109k@gmail.com>
2025-02-24 04:07:49 +0900

    chore

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
-import { Suspense, useEffect } from "react";
+import { Suspense } from "react";
 import { GithubRepositoriesTable } from "./features/github-repositories/GithubRepositoriesTable";
 import { Spinner } from "./features/spinner/Spinner";
-import { getAllRepositories } from "./shared/helpers";
 
-function App() {
-	useEffect(() => {
-		(async () => {
-			const repos = await getAllRepositories();
-			console.log({ repos });
-		})();
-	}, []);
-	return (
-		<main className="container mx-auto">
-			<h1 className="text-2xl py-4">Github Repository Manager</h1>
-			<Suspense fallback={<Spinner />}>
-				<GithubRepositoriesTable />
-			</Suspense>
-		</main>
-	);
-}
+const App = () => (
+	<main className="container mx-auto">
+		<h1 className="text-2xl py-4">Github Repository Manager</h1>
+		<Suspense fallback={<Spinner />}>
+			<GithubRepositoriesTable />
+		</Suspense>
+	</main>
+);
 
 export default App;
